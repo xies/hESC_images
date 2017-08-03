d933ded65a13cec3fcba99ab0a8518fd42c2490f
xies <xies@mit.edu>
2017-08-03 16:07:11 -0700

    fixed TIFF 16-bit saving using libtiff

--- hESC__process_07_30_17.py
+++ hESC__process_07_30_17.py
@@ -9,13 +9,19 @@ Created on Mon Jul 31 11:34:23 2017
 
 """
 
+# Core modules
 import numpy as np
 import scipy as sp
-import matplotlib.pyplot as plt
-from skimage import io, filters, morphology, measure, util, feature
-from scipy.ndimage import distance_transform_edt
 import pandas as pd
+
+# Image modules
+from skimage import io
+from libtiff import TIFFimage
+
+# Plotting modules
+import matplotlib.pyplot as plt
 import seaborn as sns
+
 
 um_per_px = 0.0000023
 
@@ -24,17 +30,17 @@ min_size = 300
 max_size = 2000
 
 # Isoperimetric threshold
-ip_threshold = 15
+ip_threshold = 17
 
 # Which experiment set to process
-which_replicate = '1'
+which_replicate = '2'
 conditions = ['DMSO','400nM_PD','9uM_RO']
 c = 0
 
 # Go through all positions available for the replicate + condition
 all_props = pd.DataFrame()
-for position in range(1):
-    position += 1
+for position in range(10):
+    position += 2
     
     # Filename base
     base = "".join(['/Users/mimi/Box Sync/hESCs/Images/7_30_2017_H9_hESC_1day_inhibitors_DAPI_Sox2488_Nanog594/',
@@ -63,12 +69,13 @@ for position in range(1):
     properties = measure_props(position,nuclei,dapi,sox,nanog)
     
     #Filter by obj size again
+    # 'I' are the 'bad' objects
     I = (properties['nuclear_area'] > max_size) | (properties['nuclear_area'] < min_size)
     
     #Filter by perimeter/area ratio
     L2 = properties['nuclear_perimeter'] ** 2
     A = properties['nuclear_area']
-    I = I | L2 / A < ip_threshold
+    I = I | (L2 / A > ip_threshold).as_matrix()
     
     # Find the proper index in the labeled image
     idx = properties.index[I] - 10000 * position
@@ -81,7 +88,18 @@ for position in range(1):
     good_labels = nuclei.copy(); good_labels[notrightsize] = 0
     nucs = properties[~I]
     
-    sp.misc.imsave(''.join([base,'nuclei.tif']),good_labels)
+    # Save the segmentation as 32-bit TIFF
+    # NB: only use the libtiff package since we need 32-bit
+    tiff = TIFFimage(good_labels, description='')
+    tiff.write_file(''.join((base, 'nuclei.tif')),
+                    compression='none')
+    # DEBUG ONLY: also save the raw segmentation
+#    tiff = TIFFimage(nuclei, description='')
+#    tiff.write_file(''.join((base, 'raw_nuclei.tif')),
+#                    compression='none')   
+
+    # Save the DataFrame as CSV
+    nucs.to_csv(''.join((base,'quantification.csv')), sep='\t')
     
     all_props = pd.concat([all_props,nucs])
     print "Done with ", position
